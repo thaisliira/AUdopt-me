df87286097aa0133d99a8407a5bec93900213b7e
Thais Lira <thais__lira@hotmail.com>
2026-02-22 22:10:09 +0000

    final adjs

--- src/app/master-page/footer/footer.ts
+++ src/app/master-page/footer/footer.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MdbFormsModule} from 'mdb-angular-ui-kit/forms';
 import {MdbRippleModule} from 'mdb-angular-ui-kit/ripple';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   imports: [
     MdbFormsModule,
-    MdbRippleModule
+    MdbRippleModule,
+    RouterLink
   ],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
